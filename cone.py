be87cc90ca739da1957bd7865e8418e1d040d325
Alexander Belsten <afbelsten@gmail.com>
2021-11-13 14:01:00 -0800

    added more points to interpolation curve

--- cone.py
+++ cone.py
@@ -15,8 +15,8 @@ def cone_M(lmbda):
     """
     Interpolate the response of the M cone for a given wavelength lmbda.
     """
-    xp = [400, 450, 500, 525, 550, 575, 600, 625, 650, 675, 700]
-    fp = [0, 0.1, 0.4, 0.9, 1, 0.8, 0.35, 0.1, 0.05, 0.001, 0]
+    xp = [400, 450,   480, 500, 525,   537, 550,  562, 575,  600, 625,  650,   675, 700]
+    fp = [  0, 0.1, 0.235, 0.4, 0.9, 0.975,   1, 0.94, 0.8, 0.35, 0.1, 0.05, 0.001,   0]
     alpha_cone = np.interp(lmbda, xp, fp)
     return alpha_cone
 
@@ -25,7 +25,7 @@ def cone_S(lmbda):
     """
     Interpolate the response of the S cone for a given wavelength lmbda.
     """
-    xp = [380, 400, 425, 447, 475, 500, 512.5, 550]
-    fp = [0, 0.05, 0.6, 0.998, 0.45, 0.1, 0.05, 0]
+    xp = [380,  400, 408, 425,  440,   477,   458,  468,  475, 500, 512.5, 550]
+    fp = [  0, 0.05,0.15, 0.6, 0.83, 0.998, 0.875,0.665, 0.45, 0.1,  0.05,   0]
     alpha_cone = np.interp(lmbda, xp, fp)
     return alpha_cone
