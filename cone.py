6438a1b2e47344c2058d81d220ccc5fa6e9172ee
Alexander Belsten <afbelsten@gmail.com>
2021-11-14 19:02:54 -0800

    updated cone activation estimations to use data from CVRL.org lab

--- cone.py
+++ cone.py
@@ -2,76 +2,38 @@ import numpy as np
 from scipy import interpolate
 
 
-def cone_L(lmbda):
-    """
-    Interpolate the response of the L cone for a given wavelength lmbda.
-    """
-    xp = [350, 400, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 710, 750, 800]
-    fp = [0, 0, 0.1, 0.3, 0.7, 0.95, 0.999, 0.9, 0.553, 0.18, 0.075, 0.001, 0, 0, 0]
-    # alpha_cone = np.interp(lmbda, xp, fp)
-    interp_cone = interpolate.interp1d(
-        xp, fp, kind="quadratic", fill_value="extrapolate"
-    )
-    alpha_cone = np.clip(interp_cone(lmbda), a_min=0.001, a_max=1)
-    return alpha_cone
+class cone():
+    def __init__(self,fname):
+        data = np.genfromtxt(fname, delimiter=',')
+        data[np.isnan(data)] = 0.
+        self.lmbda = data[:,0]
+        self.L = data[:,1]
+        self.M = data[:,2]
+        self.S = data[:,3]
+
+        
+    def cone_L(self,querylmbda):
+        """
+        Interpolate the response of the L cone for a given wavelength lmbda.
+        """
+        alpha_cone = np.interp(querylmbda, self.lmbda, self.L)
+        alpha_cone = np.clip(alpha_cone, a_min=0.0, a_max=1)
+        return alpha_cone
 
 
-def cone_M(lmbda):
-    """
-    Interpolate the response of the M cone for a given wavelength lmbda.
-    """
-    xp = [
-        350,
-        400,
-        450,
-        480,
-        500,
-        525,
-        537,
-        550,
-        562,
-        575,
-        600,
-        625,
-        650,
-        675,
-        700,
-        750,
-    ]
-    fp = [0, 0, 0.1, 0.235, 0.4, 0.9, 0.975, 1, 0.94, 0.8, 0.35, 0.1, 0.05, 0.001, 0, 0]
-    # alpha_cone = np.interp(lmbda, xp, fp)
-    interp_cone = interpolate.interp1d(
-        xp, fp, kind="quadratic", fill_value="extrapolate"
-    )
-    alpha_cone = np.clip(interp_cone(lmbda), a_min=0.001, a_max=1)
-    return alpha_cone
+    def cone_M(self,querylmbda):
+        """
+        Interpolate the response of the M cone for a given wavelength lmbda.
+        """
+        alpha_cone = np.interp(querylmbda, self.lmbda, self.M)
+        alpha_cone = np.clip(alpha_cone, a_min=0.0, a_max=1)
+        return alpha_cone
 
 
-def cone_S(lmbda):
-    """
-    Interpolate the response of the S cone for a given wavelength lmbda.
-    """
-    xp = [300, 380, 400, 408, 425, 440, 458, 468, 475, 500, 512.5, 550, 600, 700]
-    fp = [
-        0.00001,
-        0.0001,
-        0.05,
-        0.15,
-        0.6,
-        0.83,
-        0.875,
-        0.665,
-        0.45,
-        0.1,
-        0.05,
-        0.001,
-        0.001,
-        0.001,
-    ]
-    interp_cone = interpolate.interp1d(
-        xp, fp, kind="quadratic", fill_value="extrapolate"
-    )
-    alpha_cone = np.clip(
-        interp_cone(lmbda), a_min=0.001, a_max=1
-    )  # np.interp(lmbda, xp, fp)
-    return alpha_cone
+    def cone_S(self,querylmbda):
+        """
+        Interpolate the response of the S cone for a given wavelength lmbda.
+        """
+        alpha_cone = np.interp(querylmbda, self.lmbda, self.S)
+        alpha_cone = np.clip(alpha_cone, a_min=0.0, a_max=1)
+        return alpha_cone
---
+++ data/README.md
@@ -0,0 +1,2 @@
+## Data info
+The files in this directory were downloaded from [Colour & Vision Research Laboratory](http://www.cvrl.org)
